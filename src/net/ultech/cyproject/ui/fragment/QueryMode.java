b43f262eab87c26af49bd35439ea9727391bb6d2
ultmaster <scottyugochang@hotmail.com>
2014-08-23 19:39:59 +0800

    Query Mode ANR Repair

--- src/net/ultech/cyproject/ui/fragment/QueryMode.java
+++ src/net/ultech/cyproject/ui/fragment/QueryMode.java
@@ -24,91 +24,97 @@ import android.widget.TextView;
 
 public class QueryMode extends Fragment implements OnClickListener {
 
-    private EditText etWord;
-    private Button btOK;
-    private TextView tvResult;
-    private String text;
-    private MainActivity mActivity;
+	private EditText etWord;
+	private Button btOK;
+	private TextView tvResult;
+	private String text;
+	private MainActivity mActivity;
 
-    @SuppressLint("InflateParams")
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.query_layout, null);
-        etWord = (EditText) view.findViewById(R.id.qu_et_word);
-        btOK = (Button) view.findViewById(R.id.qu_bt_ok);
-        tvResult = (TextView) view.findViewById(R.id.qu_tv_result);
-        btOK.setOnClickListener(this);
-        mActivity = (MainActivity) getActivity();
-        text = mActivity.getSharedPreferences(Constants.PREFERENCE_FILE_NAME,
-                Context.MODE_PRIVATE).getString(
-                PreferenceName.STRING_LAST_QUERY, "");
-        Bundle backBundle = mActivity.mActivityStack.getBackBundle();
-        String newQueryWord = null;
-        if (backBundle != null)
-            newQueryWord = backBundle.getString("word", "");
-        if (newQueryWord != null && !TextUtils.isEmpty(newQueryWord)) {
-            text = newQueryWord;
-            etWord.setText(text);
-            etWord.invalidate();
-            btOK.performClick();
-        }
-        return view;
-    }
+	@SuppressLint("InflateParams")
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.query_layout, null);
+		etWord = (EditText) view.findViewById(R.id.qu_et_word);
+		btOK = (Button) view.findViewById(R.id.qu_bt_ok);
+		tvResult = (TextView) view.findViewById(R.id.qu_tv_result);
+		btOK.setOnClickListener(this);
+		mActivity = (MainActivity) getActivity();
+		text = mActivity.getSharedPreferences(Constants.PREFERENCE_FILE_NAME,
+				Context.MODE_PRIVATE).getString(
+				PreferenceName.STRING_LAST_QUERY, "");
+		Bundle backBundle = mActivity.mActivityStack.getBackBundle();
+		String newQueryWord = null;
+		if (backBundle != null)
+			newQueryWord = backBundle.getString("word", "");
+		if (newQueryWord != null && !TextUtils.isEmpty(newQueryWord)) {
+			text = newQueryWord;
+			etWord.setText(text);
+			etWord.invalidate();
+			btOK.performClick();
+		}
+		return view;
+	}
 
-    @Override
-    public void onStop() {
-        mActivity
-                .getSharedPreferences(Constants.PREFERENCE_FILE_NAME,
-                        Context.MODE_PRIVATE).edit()
-                .putString(PreferenceName.STRING_LAST_QUERY, text).commit();
-        super.onStop();
-    }
+	@Override
+	public void onStop() {
+		mActivity
+				.getSharedPreferences(Constants.PREFERENCE_FILE_NAME,
+						Context.MODE_PRIVATE).edit()
+				.putString(PreferenceName.STRING_LAST_QUERY, text).commit();
+		super.onStop();
+	}
 
-    @Override
-    public void onClick(View v) {
-    	mActivity.playSound("click");
-        switch (v.getId()) {
-        case R.id.qu_bt_ok:
-            String text = etWord.getText().toString().trim();
-            if (TextUtils.isEmpty(text))
-                tvResult.setText(R.string.empty_query_reminder);
-            WordInfoComplete word = CYDbDAO.findComplete(text,
-                    DatabaseHolder.getDatabase());
-            if (word != null) {
-                String source = "<b>"
-                        + getActivity().getString(R.string.query_result_title)
-                        + "</b>" + word.getName() + "<br><b>"
-                        + getActivity().getString(R.string.query_result_spell)
-                        + "</b>" + word.getSpell();
-                if (!TextUtils.isEmpty(word.getContent()))
-                    source = source
-                            + "<br><b>"
-                            + getActivity().getString(
-                                    R.string.query_result_content) + "</b>"
-                            + word.getContent();
-                if (!TextUtils.isEmpty(word.getDerivation()))
-                    source = source
-                            + "<br><b>"
-                            + getActivity().getString(
-                                    R.string.query_result_derivation) + "</b>"
-                            + word.getDerivation();
-                if (!TextUtils.isEmpty(word.getSamples()))
-                    source = source
-                            + "<br><b>"
-                            + getActivity().getString(
-                                    R.string.query_result_samples) + "</b>"
-                            + word.getSamples();
-                tvResult.setText(Html.fromHtml(source));
-            } else {
-                Bundle bundleHint = new Bundle();
-                bundleHint.putString("query", text);
-                ResultList fragmentHint = new ResultList(
-                        Mode.MODE_QUERY);
-                mActivity.mActivityStack.pushStack(bundleHint, fragmentHint);
-                mActivity.updateFragment();
-            }
-            break;
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		mActivity.playSound("click");
+		switch (v.getId()) {
+		case R.id.qu_bt_ok:
+			String text = etWord.getText().toString().trim();
+			if (TextUtils.isEmpty(text))
+				tvResult.setText(R.string.empty_query_reminder);
+			else {
+				WordInfoComplete word = CYDbDAO.findComplete(text,
+						DatabaseHolder.getDatabase());
+				if (word != null) {
+					String source = "<b>"
+							+ getActivity().getString(
+									R.string.query_result_title)
+							+ "</b>"
+							+ word.getName()
+							+ "<br><b>"
+							+ getActivity().getString(
+									R.string.query_result_spell) + "</b>"
+							+ word.getSpell();
+					if (!TextUtils.isEmpty(word.getContent()))
+						source = source
+								+ "<br><b>"
+								+ getActivity().getString(
+										R.string.query_result_content) + "</b>"
+								+ word.getContent();
+					if (!TextUtils.isEmpty(word.getDerivation()))
+						source = source
+								+ "<br><b>"
+								+ getActivity().getString(
+										R.string.query_result_derivation)
+								+ "</b>" + word.getDerivation();
+					if (!TextUtils.isEmpty(word.getSamples()))
+						source = source
+								+ "<br><b>"
+								+ getActivity().getString(
+										R.string.query_result_samples) + "</b>"
+								+ word.getSamples();
+					tvResult.setText(Html.fromHtml(source));
+				} else {
+					Bundle bundleHint = new Bundle();
+					bundleHint.putString("query", text);
+					ResultList fragmentHint = new ResultList(Mode.MODE_QUERY);
+					mActivity.mActivityStack
+							.pushStack(bundleHint, fragmentHint);
+					mActivity.updateFragment();
+				}
+				break;
+			}
+		}
+	}
 }
